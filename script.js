eaf6c66fa2ceb8122019ba5f96a153d25ee04cb4
heitornobrega <nobheitor@gmail.com>
2022-03-22 10:22:16 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
+//Adicona classe selected nas cores da paleta
+
 function select(event) {
     let color = document.getElementsByClassName('color');
-    for (c = 0; c < color.length; c++) {
+    for (let c = 0; c < color.length; c++) {
         if (color[c].classList.contains('selected')) {
             color[c].className = "color";
         }
         event.target.className = "color selected";
     }
 }
+
+//Adiciona a classe de cor ao pixel selecionado
+
 function fill(event) {
     let selected = document.getElementsByClassName('selected');
-    let pixel = document.getElementsByClassName('pixel');
     let novaClasse = '';
-    for (c = 0; c < selected.length; c++) {
+    for (let c = 0; c < selected.length; c++) {
         novaClasse = `pixel ${selected[c].id}`;
-        console.log(novaClasse);
     };
     event.target.className = novaClasse;
 }
+// Escuta evento de click proveniente do btn id="clear-board".
+let clearBoard = document.getElementById('clear-board');
+clearBoard.addEventListener('click', clear)
+
+// Substitui a classe 'pixel nomeDaCor' para 'pixel'.
+function clear(){
+    let pixel = document.getElementsByClassName('pixel');
+    for(let c = 0; c < pixel.length; c++){
+        if(pixel[c].classList.length > 1){
+            pixel[c].className = 'pixel';
+        };
+    };
+};
+
+
