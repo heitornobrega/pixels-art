a057d0657fb4d678ccd3d8dddeff30089d3a186a
heitornobrega <nobheitor@gmail.com>
2022-03-22 08:23:30 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-const colorPalette = document.getElementById('color-palette');
-// // let colors = colorPalette.children;
-// function colorSelector(event) {
-
-//     if(event.target.classList.contains('selected')){
-//         event.target.classList.remove('selected');
-//     }else{
-//         event.target.classList.add('selected');
-//     }
-// }
-
-// colorPalette.addEventListener('click', colorSelector)
-
-function select(event){
+function select(event) {
     let color = document.getElementsByClassName('color');
-    for(c = 0; c < color.length; c++){
-        if(color[c].classList.contains('selected')){
+    for (c = 0; c < color.length; c++) {
+        if (color[c].classList.contains('selected')) {
             color[c].className = "color";
         }
-    event.target.className = "color selected";
+        event.target.className = "color selected";
     }
+}
+function fill(event) {
+    let selected = document.getElementsByClassName('selected');
+    let pixel = document.getElementsByClassName('pixel');
+    let novaClasse = '';
+    for (c = 0; c < selected.length; c++) {
+        novaClasse = `pixel ${selected[c].id}`;
+        console.log(novaClasse);
+    };
+    event.target.className = novaClasse;
 }
